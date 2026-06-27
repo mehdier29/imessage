18643f1490282c4307cd272a64ce452fbc2ee9ca
mehdier29 <dejamae77@gmail.com>
2026-06-27 18:55:05 +0100

    "add all changes"

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,17 +3,43 @@ import cors from "cors";
 
 import "dotenv/config";
 
+import fs from "fs";
+import path from "path";
 
+import { clerkMiddleware } from "@clerk/express";
+
+
+
+import { connectDB } from "./lib/db.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+
+const PORT = process.env.PORT || 3000;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public");
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: FRONTEND_URL, credentials: true })); 
+app.use(clerkMiddleware());
 
+app.get("/health", (req, res) => {
+  res.status(200).json({ok: true, message: "Server is healthy"});
+});
+
+// if the public directory exists, serve the static files
+// this is for the production build
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
 })
